6a20b138182c01cb0bf718c4ce976f82a992e9d0
matthew-sullivan <contact@matthewsullivan.media>
2019-04-05 20:00:48 -0400

    Cleanup CSS and simplify JavaScript

--- composer-ribbon.js
+++ composer-ribbon.js
@@ -1,5 +1,7 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
 
+import '@polymer/iron-image/iron-image';
+
 /**
  * `composer-ribbon`
  *
@@ -17,132 +19,114 @@ class ComposerRibbon extends PolymerElement {
           --ribbon-height: 40px;
           --ribbon-colour: #555555;
         }
-        .ribbon-wrapper {
+
+        .ribbon {
           border: 1px solid var(--ribbon-colour);
           height: var(--ribbon-height);
           width: auto;
         }
-        .ribbon-image {
-          width: var(--ribbon-height);
-          height: 100%;
+
+        .ribbon__image {
           background-color: var(--ribbon-colour);
           float: left;
-          background-repeat: no-repeat;
-          background-size: contain;
+          height: 100%;
+          width: var(--ribbon-height);
         }
-        .ribbon-container {
-          height: var(--ribbon-height);
-          width: calc(100% - var(--ribbon-height));
+
+        .ribbon__container {
+          box-sizing: border-box;
           float: left;
           font-size: calc(8px + 1vh);
-          box-sizing: border-box;
-          padding-left: 10px;
-          padding-right: 10px;
+          height: var(--ribbon-height);
           line-height: var(--ribbon-height);
           overflow-x: scroll;
           overflow-y: hidden;
+          padding-left: 10px;
+          padding-right: 10px;
+          width: calc(100% - var(--ribbon-height));
           white-space: nowrap;
         }
-        .ribbon-composer {
-          float: left;
-          display: inline-block;
-        }
-        .ribbon-social {
-          display: inline-block;
 
+        .ribbon__composer {
+          display: inline-block;
+          float: left;
+        }
+
+        .ribbon__social {
           box-sizing: border-box;
+          display: inline-block;
           padding: 4px;
         }
-        a,
-        a:visited,
-        a:hover,
-        a:active {
-          -webkit-backface-visibility: hidden;
-          backface-visibility: hidden;
+
+        .ribbon__title {
+          color: var(--ribbon-colour);
           position: relative;
-          transition: 0.5s color ease;
           text-decoration: none;
-          color: var(--ribbon-colour);
         }
-        a:hover {
-          color: var(--ribbon-colour);
-        }
-        a.before:before,
-        a.after:after {
-          content: '';
-          transition: 0.5s all ease;
-          -webkit-backface-visibility: hidden;
+
+        .ribbon__title:after {
           backface-visibility: hidden;
-          position: absolute;
-        }
-        a.before:before {
-          top: -0.25em;
-        }
-        a.after:after {
+          background: var(--ribbon-colour);
           bottom: -0.25em;
-        }
-        a.before:before,
-        a.after:after {
+          content: '';
           height: 1px;
           width: 0;
-          background: var(--ribbon-colour);
-        }
-        a.composer-title:after {
           left: 50%;
-          -webkit-transform: translateX(-50%);
+          position: absolute;
           transform: translateX(-50%);
+          transition: 0.5s all ease;
+          -webkit-backface-visibility: hidden;
+          -webkit-transform: translateX(-50%);
         }
-        a.before:hover:before,
-        a.after:hover:after {
+
+        .ribbon__title:hover:after {
           width: 100%;
         }
-        .no-link {
-          pointer-events: none;
+
+        .ribbon__title--disabled {
           cursor: default;
+          pointer-events: none;
         }
-        ul {
-          list-style-type: none;
-          margin: 0;
-          padding: 0;
-        }
-        li {
+
+        .ribbon__icons {
           display: inline;
           margin-left: 4px;
         }
       </style>
-      <div class="ribbon-wrapper">
-        <div
-          class="ribbon-image"
-          style="background-image: url({{image}})"
-        ></div>
-        <div class="ribbon-container">
-          <div class="ribbon-composer">
+
+      <div class="ribbon">
+        <iron-image
+          class="ribbon__image"
+          sizing="contain"
+          src="{{image}}"
+        ></iron-image>
+
+        <div class="ribbon__container">
+          <div class="ribbon__composer">
             <a
+              class="ribbon__title"
               href="{{url}}"
               id="ribbonUrl"
-              class="composer-title after"
               target="_blank"
               >{{name}}</a
             >
           </div>
-          <div class="ribbon-social">
-            <ul>
-              <template is="dom-repeat" items="{{social}}" as="social">
-                <li>
-                  <a
-                    href="{{social.link}}"
-                    class="composer-title after"
-                    target="_blank"
-                  >
-                    <social-media-icons
-                      icon="{{social.icon}}"
-                      color="{{colour}}"
-                      size="18"
-                    ></social-media-icons>
-                  </a>
-                </li>
-              </template>
-            </ul>
+
+          <div class="ribbon__social">
+            <template
+              as="social"
+              class="ribbon__icons"
+              items="{{social}}"
+              is="dom-repeat"
+            >
+              <a class="ribbon__title" href="{{social.link}}" target="_blank">
+                <!-- <social-media-icons
+                  color="{{colour}}"
+                  icon="{{social.icon}}"
+                  size="18"
+                ></social-media-icons> -->
+              </a>
+            </template>
           </div>
         </div>
       </div>
@@ -155,83 +139,55 @@ class ComposerRibbon extends PolymerElement {
 
   static get properties() {
     return {
-      name: {
-        type: String,
-        value: 'Composer Name',
-        notify: true,
-        reflectToAttribute: true,
-        observer: '_setComposerName',
-      },
-      url: {
-        type: String,
-        value: '#',
-        notify: true,
-        reflectToAttribute: true,
-        observer: '_setComposerUrl',
-      },
-      image: {
-        type: String,
-        value: '../assets/img/default-icon.png',
-        notify: true,
-        reflectToAttribute: true,
-        observer: '_setComposerImage',
-      },
-      social: {
-        type: Array,
-        value() {
-          return [];
-        },
-        notify: true,
-        reflectToAttribute: true,
-        observer: '_setComposerSocial',
-      },
       colour: {
         type: String,
-        value: '#555555',
-        notify: true,
-        reflectToAttribute: true,
-        observer: '_setComposerColour',
+        value: '#555',
       },
       height: {
         type: String,
         value: '40px',
-        notify: true,
-        reflectToAttribute: true,
-        observer: '_setComposerHeight',
       },
-      _setComposerName: (name) => {
-        return name;
+      image: {
+        type: String,
+        value: '../assets/img/default-icon.png',
       },
-      _setComposerUrl: (url) => {
-        return url;
+      name: {
+        type: String,
+        value: 'Composer Name',
       },
-      _setComposerImage: (path) => {
-        return path;
+      social: {
+        type: Array,
+        value: () => {
+          return [];
+        },
       },
-      _setComposerSocial: (social) => {
-        return social;
-      },
-      _setComposerColour: (colour) => {
-        return colour;
-      },
-      _setComposerHeight: (height) => {
-        return height;
+      url: {
+        type: String,
+        value: '#',
       },
     };
   }
-  ready() {
-    super.ready();
+
+  connectedCallback() {
+    super.connectedCallback();
+
+    this._setRibbonUrl();
 
     this.updateStyles({
       '--ribbon-colour': this.colour,
       '--ribbon-height': this.height,
     });
+  }
 
-    if (this.url == '#') {
-      const ribbonUrl = this.$.ribbonUrl;
-
-      ribbonUrl.classList.add('no-link');
+  /*
+   * Set ribbon url
+   */
+  _setRibbonUrl() {
+    if (this.url !== '#') {
+      return;
     }
+
+    this.$.ribbonUrl.classList.add('ribbon__title--disabled');
   }
 }
 window.customElements.define(ComposerRibbon.is, ComposerRibbon);
